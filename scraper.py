9c576b261af9a282369f0aa87d8538751cd3c182
oofidc <70336313+oofidc@users.noreply.github.com>
2025-04-02 18:36:11 -0500

    Add Error Catching to get_name_info method to YorubaNames

--- scraper.py
+++ scraper.py
@@ -22,30 +22,42 @@ def get_all_names():
         names.append(names_by_first_char(char))
     return names
 
+
+    
+
 def get_name_info(name):
     url = "https://www.yorubaname.com/entries/" + name
     page = requests.get(url=url).text
     soup = bs4(page,"html.parser")
-
     info_dict = {}
-    #soup.select(soup.find("div",class_="col-sm-7"))
-    info_dict["Name"] = soup.find("h2",id="name-entry").text #Returns version of name(with diacritics) listed, not (possibly) non-diacrticized version
-    info_dict["Meaning"] = soup.find("h4",string=re.compile(r'Meaning of .*')).find_next('p').text
-    info_dict["Extended Meaning"] = soup.find("h4",string=re.compile(r'Extended Meaning')).find_next('p').text
-    info_dict["Morphology"] = soup.find("h4",string=re.compile(r'Morphology')).find_next('p').text
+
+    #Element var used to store the element before it is checked whether it is None to avoid errors
+    element = soup.find("h2",id="name-entry")
+    info_dict["Name"] = element.text #Returns version of name(with diacritics) listed, not (possibly) non-diacrticized version
     
-    #Get Different Parts of Gloss - I'm not sure if this method always works but SHOULD BE TESTED
-    num_parts = len(info_dict['Morphology'].split('-'))# Note I can't just search for each seperate part - wouldn't work for cases like https://www.yorubaname.com/entries/Fa%CC%81di%CC%80mu%CC%81la%CC%80 where Morphology and Gloss parts don't exactly match
-    start_point = soup.find("h4",string=re.compile(r'Gloss'))
-    parts = []
-    for i in range(0,num_parts):
-        parts.append(start_point.find_next("strong").text + start_point.find_next("span").text)
-        start_point = start_point.find_next("span")
+    element = soup.find("h4",string=re.compile(r'Meaning of .*'))
+    if element is not None: info_dict["Meaning"] = element.find_next('p').text
     
-    info_dict["Gloss"] = parts
+    element =  soup.find("h4",string=re.compile(r'Extended Meaning'))
+    if element is not None: info_dict["Extended Meaning"] = element.find_next('p').text
+    
+    element = soup.find("h4",string=re.compile(r'Morphology'))
+    if element is not None: info_dict["Morphology"] = element.find_next('p').text
+    
+    if soup.find("h4",string=re.compile(r'Gloss')) is not None:
+        #Get Different Parts of Gloss - I'm not sure if this method always works but SHOULD BE TESTED
+        num_parts = len(info_dict['Morphology'].split('-'))# Note I can't just search for each seperate part - wouldn't work for cases like https://www.yorubaname.com/entries/Fa%CC%81di%CC%80mu%CC%81la%CC%80 where Morphology and Gloss parts don't exactly match
+        start_point = soup.find("h4",string=re.compile(r'Gloss'))
+        parts = []
+        for i in range(0,num_parts):
+            parts.append(start_point.find_next("strong").text + start_point.find_next("span").text)
+            start_point = start_point.find_next("span")
+        
+        info_dict["Gloss"] = parts
 
     #get Geolocation by finding Geolocation header, then paragraph, then using a regex search to capture the text from "Common in: X"
-    info_dict["Geolocation"] = re.search(r'Common in:\s*(\w+)',soup.find("h4",string=re.compile(r'Geolocation')).find_next('p').text).group(1)
+    if soup.find("h4",string=re.compile(r'Geolocation')):
+        info_dict["Geolocation"] = re.search(r'Common in:\s*(\w+)',soup.find("h4",string=re.compile(r'Geolocation')).find_next('p').text).group(1)
     info_dict["Pronunciation_URL"] = f"https://tts.yorubaname.com/api/v1/names/{info_dict["Name"]}"
     return info_dict
 
